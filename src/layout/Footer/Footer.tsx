6ad0939bca5731a5b62081da8df858e1f42548f7
Nguyễn Giang <giangnt.it0101@gmail.com>
2022-12-29 15:49:00 +0700

    update gg map

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,15 +1,15 @@
-import classNames from "classnames/bind";
-import styles from "./Footer.module.scss";
-import { FaFacebookF, FaYoutube, FaGooglePlusG } from "react-icons/fa";
-import { useEffect, useState } from "react";
-import { getCompany, getContact } from "../../api/fetchApi";
+import classNames from 'classnames/bind';
+import styles from './Footer.module.scss';
+import { FaFacebookF, FaYoutube, FaGooglePlusG } from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { getCompany, getContact } from '../../api/fetchApi';
 const cx = classNames.bind(styles);
 
 function Footer() {
-  const [logo, setLogo] = useState<string>("");
-  const [add, setAdd] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [phone, setPhone] = useState<string>("");
+  const [logo, setLogo] = useState<string>('');
+  const [add, setAdd] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
 
   useEffect(() => {
     (async () => {
@@ -29,28 +29,24 @@ function Footer() {
   }, []);
 
   return (
-    <footer className={cx("footer")}>
+    <footer className={cx('footer')}>
       <div className="flex flex-col tablet:flex-row justify-center">
         <div className=" p-[15px]">
           <h1>
             <img
               className="w-[120px] py-5"
-              src={process.env.HOST_NAME_API + "/" + logo}
+              src={process.env.HOST_NAME_API + '/' + logo}
               alt="Công ty giải pháp công nghệ v-fast"
             />
           </h1>
+          <p>- {add}</p>
           <p>
-            - {add}
-          </p>
-          <p>
-            <a href={`mailto:${email}`}>
-              - Email: {email}
-            </a>
+            <a href={`mailto:${email}`}>- Email: {email}</a>
           </p>
           <p>
             <a href={`tel:${phone}`}>- Phone: {phone}</a>
           </p>
-          <div className={cx("contact-icon")}>
+          <div className={cx('contact-icon')}>
             <a href="#" className="text-[#3e1cfd]">
               <FaFacebookF />
             </a>
@@ -64,7 +60,7 @@ function Footer() {
         </div>
         <div className="p-[15px]  laptop:w-[600px]">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7448.125488635246!2d105.78844071950445!3d21.030175303408523!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe706fbbcb6c30c88!2sThe%20Park%20Home!5e0!3m2!1svi!2s!4v1657869458279!5m2!1svi!2s"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d391.42110145330497!2d105.80055417639613!3d21.038844243334406!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab3edb573115%3A0x621f4ddb16883760!2zMjFhIE5nw7UgMTU4IC0gTmd1eeG7hW4gS2jDoW5oIFRvw6BuLCBRdWFuIEhvYSwgQ-G6p3UgR2nhuqV5LCBIw6AgTuG7mWkgMTIyMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1672303288908!5m2!1svi!2s"
             width="100%"
             height="200"
             style={{ border: 0 }}
